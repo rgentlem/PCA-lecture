2d4bdd5eedcfa042b918f492ff570f99610a315a
Robert Gentleman <rgentlem@gmail.com>
2024-10-30 17:51:37 -0400

    added in the html output, and information on packages used

---
+++ getPkgInfo.R
@@ -0,0 +1,14 @@
+## from ChatGPT - 
+
+# Get package info from session
+sI <- sessionInfo()
+
+pkg_info = sI$otherPkgs
+# Extract package names and versions
+pkg_df <- data.frame(
+  Package = names(pkg_info),
+  Version = sapply(pkg_info, function(pkg) pkg$Version)
+)
+
+# Save to a CSV file
+write.csv(pkg_df, "package_versions.csv", row.names = FALSE)
